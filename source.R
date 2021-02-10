deb55ad142d2c52d7fb780afa6d3cf7365e8307d
LOGAN MCKERLICH <Logan.mckerlich@gmail.com>
2021-02-10 10:01:54 -0800

    updated list and pic

--- source.R
+++ source.R
@@ -1,22 +1,31 @@
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
+library(maps)
 desire<-read.csv("san.dan.csv")
 colnames(desire)
 
 desire<-read.csv("san.dan.csv")
-colnames(desire)
+
 desire<-desire %>% 
   rename("Time" = estimated.time..hours.,
          "Desire" = desire.1.10.,
          "Activity" = activity)
+
 plot_desire<-ggplot(data = desire)+
   geom_point(mapping = aes(x=Time,
                            y=Desire,
                            color = Activity),
              size = 7)
+
 table<-kable(desire)
 
 total<- nrow(desire)
 
 tot.time<-sum(desire$Time)
+
+want_most<-desire %>% 
+  filter(Desire == max(Desire)) %>% 
+  select(Activity)
+
+
